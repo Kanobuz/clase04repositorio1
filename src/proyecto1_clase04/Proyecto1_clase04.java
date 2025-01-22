eaa1302f091af72965eb613606c5e3f8e3a2b644
Said Huertas <shuertas1992@gmail.com>
2025-01-21 20:06:59 -0500

    Carga inicial

---
+++ src/proyecto1_clase04/Proyecto1_clase04.java
@@ -0,0 +1,20 @@
+package proyecto1_clase04;
+import java.util.Scanner;
+
+public class Proyecto1_clase04 {
+
+    public static void main(String[] args) {
+        // Declarar variables
+        int n1, n2, suma;
+        Scanner lectura = new Scanner(System.in);
+        // Entra de datos
+        System.out.print("Ingrese número 1: ");
+        n1=lectura.nextInt();
+        System.out.print("Ingrese número 2: ");
+        n2=lectura.nextInt();
+        // Proceso de datos
+        suma = n1 + n2;
+        // Salida de datos
+        System.out.println("La suma de los número es: " + suma);
+    }
+}
