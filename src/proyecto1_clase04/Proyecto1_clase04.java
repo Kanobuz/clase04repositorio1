acd153f88e3b32bd1c21f3be07c80dc5b0084fc7
Said Huertas <shuertas1992@gmail.com>
2025-01-21 20:19:14 -0500

    Se agrega nro 3

--- src/proyecto1_clase04/Proyecto1_clase04.java
+++ src/proyecto1_clase04/Proyecto1_clase04.java
@@ -5,15 +5,17 @@ public class Proyecto1_clase04 {
 
     public static void main(String[] args) {
         // Declarar variables
-        int n1, n2, suma;
+        int n1, n2, n3, suma;
         Scanner lectura = new Scanner(System.in);
         // Entra de datos
         System.out.print("Ingrese número 1: ");
         n1=lectura.nextInt();
         System.out.print("Ingrese número 2: ");
         n2=lectura.nextInt();
+        System.out.print("Ingrese número 3: ");
+        n3=lectura.nextInt();
         // Proceso de datos
-        suma = n1 + n2;
+        suma = n1 + n2 + n3;
         // Salida de datos
         System.out.println("La suma de los número es: " + suma);
     }
